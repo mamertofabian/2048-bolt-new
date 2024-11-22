2c812a1c91a297f2fdf413a438687963aa92b2c4
Mamerto Fabian Jr <mfabianjr@gmail.com>
2024-11-22 11:30:24 +0800

    Update Grid.tsx

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function Grid() {
   return (
-    <div className="relative grid grid-cols-4 gap-2 bg-[#bbada0] rounded-md p-2">
+    <div className="grid grid-cols-4 gap-4 bg-[#bbada0] rounded-md p-4">
       {[...Array(16)].map((_, i) => (
         <div
           key={i}
-          className="w-[72px] h-[72px] bg-[#cdc1b4] rounded-md"
+          className="w-16 h-16 bg-[#cdc1b4] rounded-md"
         />
       ))}
     </div>
